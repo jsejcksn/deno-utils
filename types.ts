7638610c0430eae546991ff9597fc845543737c0
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-02-14 01:25:19 -0600

    feat(types): add Fn

--- types.ts
+++ types.ts
@@ -1,2 +1,6 @@
+// deno-lint-ignore no-explicit-any
+export type Fn<Params extends readonly any[] = readonly any[], Result = any> =
+  (...params: Params) => Result;
+
 export type Maybe<T> = T | null;
 export type OrPromise<T> = T | Promise<T>;
