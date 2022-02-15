ee284a97db17d8003e5d2bc8c70be9f7ebf048ed
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-02-14 20:52:38 -0600

    feat: add types: Ctor, Values

--- types.ts
+++ types.ts
@@ -1,6 +1,11 @@
 // deno-lint-ignore no-explicit-any
+export type Ctor<Params extends readonly any[] = readonly any[], Result = any> =
+  new (...params: Params) => Result;
+
+// deno-lint-ignore no-explicit-any
 export type Fn<Params extends readonly any[] = readonly any[], Result = any> =
   (...params: Params) => Result;
 
 export type Maybe<T> = T | null;
 export type OrPromise<T> = T | Promise<T>;
+export type Values<T> = T[keyof T];
