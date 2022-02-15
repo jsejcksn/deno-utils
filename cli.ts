9548a0dff51d16e340a35d3087708683ca8e8e0d
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-02-14 20:54:57 -0600

    refactor: prune abstraction

--- cli.ts
+++ cli.ts
@@ -1,16 +1,17 @@
 // deno-lint-ignore no-explicit-any
 type IsArrayGeneric = <T>(_: T) => _ is Extract<T, readonly any[]>;
-const isArray = Array.isArray.bind(Array) as IsArrayGeneric;
 
 /** Logs message to stderr/stdout and terminates the program */
 export function printAndExit (message: string, code?: number): never;
 /** Logs messages to stderr/stdout and terminates the program */
 export function printAndExit (messages: readonly string[], code?: number): never;
 export function printAndExit (
-  messages: string | readonly string[],
+  msgOrMsgs: string | readonly string[],
   code = 1,
 ): never {
   const fn = console[code === 0 ? 'log' : 'error'].bind(console);
-  for (const msg of isArray(messages) ? messages : [messages]) fn(msg);
+  const messages =
+    (Array.isArray as IsArrayGeneric)(msgOrMsgs) ? msgOrMsgs : [msgOrMsgs];
+  for (const msg of messages) fn(msg);
   Deno.exit(code);
 }
