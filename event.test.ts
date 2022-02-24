f9376a711cd5a13902301214acbeeb9f3dc875ab
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-02-23 20:21:44 -0600

    feat(event): add CustomEventTarget class
    - Needs tests

--- event.test.ts
+++ event.test.ts
@@ -1,5 +1,5 @@
-import {assertStrictEquals} from './test_deps.ts';
-import {createEvent} from './event.ts';
+import {assertStrictEquals, unimplemented} from './test_deps.ts';
+import {createEvent, CustomEventTarget} from './event.ts';
 
 Deno.test('createEvent', async (ctx) => {
   await ctx.step('created event has correct property values', () => {
@@ -14,4 +14,9 @@ Deno.test('createEvent', async (ctx) => {
     assertStrictEquals(ev.type, eventType);
     assertStrictEquals(ev.detail, eventDetail);
   });
+});
+
+Deno.test('CustomEventTarget', async (ctx) => {
+  unimplemented();
+  await ctx.step('', () => {});
 });
--- event.ts
+++ event.ts
@@ -1,9 +1,69 @@
-export type TypedCustomEvent<Type extends string, Detail> =
+import type {Fn, Values} from './types.d.ts';
+
+export type TypedCustomEvent<Type extends string, Detail = unknown> =
   CustomEvent<Detail> & {type: Type};
 
 export function createEvent <Type extends string, Detail>(
   type: Type,
-  detail: Detail,
+  detail?: Detail,
+  init?: Omit<CustomEventInit, 'detail'>,
 ): TypedCustomEvent<Type, Detail> {
-  return new CustomEvent(type, {detail}) as TypedCustomEvent<Type, Detail>;
+  const evInit = {...init, detail};
+  return new CustomEvent(type, evInit) as TypedCustomEvent<Type, Detail>;
+}
+
+export type CustomEventCallback<Type extends string = string, Detail = unknown> =
+  Fn<[event: TypedCustomEvent<Type, Detail>], void>;
+
+// deno-lint-ignore no-explicit-any
+export type EventCallbackFromCustomEvent<T extends TypedCustomEvent<string, any>> =
+  Fn<[event: T], void>;
+
+export type CustomEventMap = Record<string, CustomEvent>;
+
+export class CustomEventTarget<T extends CustomEventMap = Record<never, never>> extends EventTarget {
+  // deno-lint-ignore ban-ts-comment
+  // @ts-ignore
+  addEventListener <K extends keyof T & string>(
+    type: K,
+    callback: EventCallbackFromCustomEvent<T[K]>,
+    options?: Parameters<EventTarget['addEventListener']>[2],
+  ): void {
+    return super.addEventListener(
+      type,
+      callback as Extract<Parameters<EventTarget['addEventListener']>[1], Fn>,
+      options,
+    );
+  }
+
+  dispatch <K extends keyof T & string>(
+    type: K,
+    ...[detail]: (
+      unknown extends T[K]['detail'] ? [detail?: unknown]
+      : T[K]['detail'] extends undefined ? [detail?: undefined]
+      : T[K]['detail'] extends never ? []
+      : [detail: T[K]['detail']]
+    )
+  ): void {
+    const event = createEvent(type, detail) as unknown as Values<T>;
+    this.dispatchEvent(event);
+  }
+
+  dispatchEvent <E extends Values<T>>(event: E): boolean {
+    return super.dispatchEvent(event);
+  }
+
+  // deno-lint-ignore ban-ts-comment
+  // @ts-ignore
+  removeEventListener <K extends keyof T & string>(
+    type: K,
+    callback: EventCallbackFromCustomEvent<T[K]>,
+    options?: Parameters<EventTarget['removeEventListener']>[2],
+  ): void {
+    return super.removeEventListener(
+      type,
+      callback as Extract<Parameters<EventTarget['removeEventListener']>[1], Fn>,
+      options,
+    );
+  }
 }
